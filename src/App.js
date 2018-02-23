0ddcb32ca8fba4e1df9919ea14b5da7297005afd
Viktor Moyseyenko <Viktor.Moyseyenko@synapse.com>
2018-02-23 13:19:48 +0200

    EDIT: syntax

--- src/App.js
+++ src/App.js
@@ -63,15 +63,16 @@ class App extends Component {
   }
 
   render () {
-    return <FilmPage 
-              films={this.state.films} 
-              form={this.state.form} 
-              selectFilm={this.selectFilm} 
-              changeFormValue={this.changeFormValue} 
-              submit={this.submit}
-              cancel={this.cancel}
-              changeImage={this.changeImage}
-            />
+    return 
+    <FilmPage 
+      films={this.state.films} 
+      form={this.state.form} 
+      selectFilm={this.selectFilm} 
+      changeFormValue={this.changeFormValue} 
+      submit={this.submit}
+      cancel={this.cancel}
+      changeImage={this.changeImage}
+    />
   }
 };
 
