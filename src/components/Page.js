cc73acc6390d4afb0f79bcf053d73198fb4d772f
Viktor Moyseyenko <vityok@gmail.com>
2018-02-26 10:50:15 +0200

    ADD: settings page

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Grid} from 'react-bootstrap'
+import { observer, inject } from 'mobx-react';
 
 import FilmPage from './FilmPage/FilmPage'
 import Menu from './Menu/Menu'
+import SettingsPage from './SettingsPage/SettingsPage'
 
 
-const Page = () => 
+const Page = ({siteStore}) => 
   <Grid>
     <Menu/>
-    <FilmPage/>
+    {/* ### Site sections ### */}
+    {/* FILMS */}
+    {siteStore.currentSection === 'films' && <FilmPage/>}
+    {/* SETTINGS */}
+    {siteStore.currentSection === 'setting' && <SettingsPage/>}
   </Grid>;
     
-export default Page
+export default inject('siteStore')(observer(Page))
---
+++ src/components/SettingsPage/SettingsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Col, Row, Alert} from 'react-bootstrap' // eslint-disable-line no-unused-vars
+
+
+const SettingsPage = () => 
+  <Row>
+    <Alert>
+      Stub for settings page --- easy-easy-real-talk!...
+    </Alert>
+  </Row>;
+    
+export default SettingsPage
